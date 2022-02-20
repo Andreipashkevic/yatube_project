7ae972fd8b4bcb3b7744aab0ead950fae7c00014
Андрей Пашкевич <pashkevich.av2@gmail.com>
2022-02-20 11:55:05 +0300

    1.01

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,11 +4,26 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    description = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Post(models.Model):
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True, null=True
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts'
-    ) 
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#from turtle import title
+from re import template
+from turtle import title
+from django.shortcuts import get_object_or_404, render
+from .models import Post, Group
 
-# Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    #title = 'Главная страница'
+    #text = 'Последние обновления'
+    context = {
+        'posts': posts,
+        #'title': title,
+        #'text': text,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
